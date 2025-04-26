24128885257e84596fb8be306aa4ffaff0fbf1bd
Bang-jun00 <qudwns5250@naver.com>
2025-04-27 08:18:43 +0900

    [Feat]몬스터 애니메이션 추가 및 기능 구현

---
+++ Assets/Script/EnemyAnimation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    public Transform enemySprite;
+
+    [SerializeField] float speed;
+
+    [SerializeField] float minSize, maxSize;
+
+    private float activeSize;
+    
+    void Start()
+    {
+        activeSize = maxSize;
+
+        speed = speed * Random.Range(0.75f, 1.25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemySprite.localScale = Vector3.MoveTowards(enemySprite.localScale, Vector3.one * activeSize, speed * Time.deltaTime);
+
+
+           
+
+        if(enemySprite.localScale.x == activeSize)
+        {
+            if(activeSize == maxSize)
+            {
+                activeSize = minSize;
+            }
+            else
+            {
+                activeSize = maxSize;
+            }
+        }
+    }
+}
