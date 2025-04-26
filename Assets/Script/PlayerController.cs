3350809dd68fa9834eadb00c5de64c0b0b4c775a
Bang-jun00 <qudwns5250@naver.com>
2025-04-26 16:12:26 +0900

    [Feat] 플레이어 오브젝트 생성 및 움직임 기능 구현

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    
+
+    
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        //벡터 값 초기화
+        Vector3 moveInput = new Vector3(0f, 0f, 0f);
+
+        //수평
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+
+        //수직
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        //오브젝트의 위치에 방향 속도 시간보정을 더해줌
+        transform.position += (moveInput * moveSpeed * Time.deltaTime);
+
+    }
+
+}
