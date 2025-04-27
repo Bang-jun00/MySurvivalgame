d393434c1c8a1dcaed3cfe248f6c1ae281dd294a
Bang-jun00 <qudwns5250@naver.com>
2025-04-27 12:11:11 +0900

    [Feat]Enemy를 담는 pool 기능 구현 및 스포너 코드 리펙토링

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     [Header("Spawner")]
-    public GameObject enemySpawn; //스폰할 프리팹
+    public EnemyPool enemyPool; // 몹 프리팹이 저장되어있는 풀 연결
     public float timeToSpawn = 2f; //적이 생성되는 간격
     private float spawnTimer; //타이머
 
@@ -13,11 +14,17 @@ public class EnemySpawner : MonoBehaviour
     public Transform minSpawn, maxSpawn;
     public Transform target;
 
+    private float dispawnDistance;
+    private List<GameObject> spawnEnemies = new List<GameObject>(); //활성화된 몹을 담을 리스트 추가
+    
+
     void Start()
     {
         spawnTimer = timeToSpawn;
         
         target  = PlayerHP.instance.transform; //싱글톤에서 플레이어 Transform 가져오기
+
+        dispawnDistance = Vector3.Distance(transform.position, maxSpawn.position) + 4f;
     }
 
     
@@ -29,10 +36,31 @@ public class EnemySpawner : MonoBehaviour
         {
             spawnTimer = timeToSpawn;
 
-            Instantiate(enemySpawn, SelectSpawnPoint(), transform.rotation); //적 생성
+            SpawnEnemy(); //적 생성
         }
 
-        transform.position = target.position; // 스포너가 player를 따라다님   
+        transform.position = target.position; // 스포너가 player를 따라다님
+        
+        //적 디스폰 체크
+        for (int i = spawnEnemies.Count - 1; i >= 0; i--)
+        {
+            GameObject enemy = spawnEnemies[i];
+            if (Vector3.Distance(target.position, enemy.transform.position) > dispawnDistance)
+            {
+                enemyPool.ReturnEnemy(enemy); // 풀에 돌려주고
+                spawnEnemies.RemoveAt(i); // 리스트에서도 제거
+            }
+        }
+
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject enemy = enemyPool.GetEnemy();
+        enemy.transform.position = SelectSpawnPoint();
+        enemy.transform.rotation = transform.rotation;
+
+        spawnEnemies.Add(enemy); //리스트에 추가
     }
 
     public Vector3 SelectSpawnPoint()
