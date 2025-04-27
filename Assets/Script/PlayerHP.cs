c37df8f211fd06801c592e52ccfd140458b3dee0
Bang-jun00 <qudwns5250@naver.com>
2025-04-27 09:54:28 +0900

    [Fix]캐릭터 애니메이션이 체력바에 적용되는 버그 수정

--- Assets/Script/PlayerHP.cs
+++ Assets/Script/PlayerHP.cs
@@ -1,27 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
+    //싱글톤 설정
     public static PlayerHP instance;    
-    
-    [SerializeField] float currentHealth, maxHealth;
-
     private void Awake()
     {
         instance = this;
     }
 
+    [Header("Health")]
+    [SerializeField] float currentHealth, maxHealth;
+    
+    public Slider healthSlider; //체력바 연결할 변수
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        healthSlider.maxValue = maxHealth; // Max 설정
+        healthSlider.value = currentHealth;// value(현재값) 설정
     }
+        
 
     public void TakeDamage(float damage)
     { 
         currentHealth -= damage;
+
+        healthSlider.value = currentHealth; //데미지 입으면 체력바 업데이트
 
         if (currentHealth <= 0)
         {
